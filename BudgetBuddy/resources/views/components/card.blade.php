c2386c31fdc9ad85b02bc27bb0c2e6cb6b21c407
Harsh Doliya <137301163+CodeJoc@users.noreply.github.com>
2025-03-06 01:50:46 +0530

    Delete BudgetBuddy directory

--- BudgetBuddy/resources/views/components/card.blade.php
+++
@@ -1,7 +0,0 @@
-@props([
-    'class' => ''
-])
-
-<div @class(['rounded-lg shadow-xl overflow-clip bg-white', $class])>
-    {{ $slot }}
-</div>
--- BudgetBuddy/resources/views/components/textarea.blade.php
+++
@@ -1,2 +0,0 @@
-<textarea class="w-full h-full focus:border-accent shadow-inner text-sm py-1 px-2.5 rounded border resize-none outline-none"
-    {{ $attributes }}></textarea>
